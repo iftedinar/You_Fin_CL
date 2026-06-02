f5dcf16a751a0b8951356428a349a193aa9dea71
Iftekhar Dinar <iftekharifty42@gmail.com>
2026-06-02 10:11:22 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,5 +6,5 @@ export default async function RootPage() {
   const { data: { user } } = await supabase.auth.getUser()
 
   if (user) redirect('/library')
-  else redirect('/auth/login')
+  else redirect('/login')
 }
